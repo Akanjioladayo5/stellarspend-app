03817f7eb0133f9297d37ae34beab42b9d3e6985
OlufunbiIK <olufunbiibrahim@gmail.com>
2026-07-27 03:08:43 +0100

    fix

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,13 +1,13 @@
 /// <reference types="cypress" />
 
 declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace Cypress {
     interface Chainable {
       mockFreighter(): Chainable<void>;
       mockStellarAPI(): Chainable<void>;
     }
   }
-
   interface Window {
     freighter?: {
       isConnected: () => Promise<boolean>;
